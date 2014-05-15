a4cd7da86c8ce1c9fa8a5ac9184344f0abb3b702
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2014-05-15 20:27:37 +0900

    SQL実行機能。

---
+++ src/main/java/cherry/sqlapp/controller/secure/sqlexec/SqlExecCsvForm.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2014 agwlvssainokuni
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cherry.sqlapp.controller.secure.sqlexec;
+
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.web.multipart.MultipartFile;
+
+import cherry.sqlapp.controller.BaseForm;
+import cherry.sqlapp.validation.SqlStatementSize;
+
+public class SqlExecCsvForm extends BaseForm {
+
+	private static final long serialVersionUID = 1L;
+
+	@NotEmpty
+	@SqlStatementSize
+	private String sql;
+
+	@SqlStatementSize
+	private String sqlAlt1;
+
+	@SqlStatementSize
+	private String sqlAlt2;
+
+	private MultipartFile file;
+
+	public String getSql() {
+		return sql;
+	}
+
+	public void setSql(String sql) {
+		this.sql = sql;
+	}
+
+	public String getSqlAlt1() {
+		return sqlAlt1;
+	}
+
+	public void setSqlAlt1(String sqlAlt1) {
+		this.sqlAlt1 = sqlAlt1;
+	}
+
+	public String getSqlAlt2() {
+		return sqlAlt2;
+	}
+
+	public void setSqlAlt2(String sqlAlt2) {
+		this.sqlAlt2 = sqlAlt2;
+	}
+
+	public MultipartFile getFile() {
+		return file;
+	}
+
+	public void setFile(MultipartFile file) {
+		this.file = file;
+	}
+
+}
---
+++ src/main/java/cherry/sqlapp/controller/secure/sqlexec/SqlExecSelectForm.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2014 agwlvssainokuni
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cherry.sqlapp.controller.secure.sqlexec;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import cherry.sqlapp.controller.BaseForm;
+import cherry.sqlapp.validation.SqlClauseSize;
+
+public class SqlExecSelectForm extends BaseForm {
+
+	private static final long serialVersionUID = 1L;
+
+	@NotEmpty
+	@SqlClauseSize
+	private String select;
+
+	@NotEmpty
+	@SqlClauseSize
+	private String from;
+
+	@SqlClauseSize
+	private String where;
+
+	@SqlClauseSize
+	private String groupBy;
+
+	@SqlClauseSize
+	private String having;
+
+	@SqlClauseSize
+	private String orderBy;
+
+	private Integer limit;
+
+	private Integer offset;
+
+	public String getSelect() {
+		return select;
+	}
+
+	public void setSelect(String select) {
+		this.select = select;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public String getWhere() {
+		return where;
+	}
+
+	public void setWhere(String where) {
+		this.where = where;
+	}
+
+	public String getGroupBy() {
+		return groupBy;
+	}
+
+	public void setGroupBy(String groupBy) {
+		this.groupBy = groupBy;
+	}
+
+	public String getHaving() {
+		return having;
+	}
+
+	public void setHaving(String having) {
+		this.having = having;
+	}
+
+	public String getOrderBy() {
+		return orderBy;
+	}
+
+	public void setOrderBy(String orderBy) {
+		this.orderBy = orderBy;
+	}
+
+	public Integer getLimit() {
+		return limit;
+	}
+
+	public void setLimit(Integer limit) {
+		this.limit = limit;
+	}
+
+	public Integer getOffset() {
+		return offset;
+	}
+
+	public void setOffset(Integer offset) {
+		this.offset = offset;
+	}
+
+}
