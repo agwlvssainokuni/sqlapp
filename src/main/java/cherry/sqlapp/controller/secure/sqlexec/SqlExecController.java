5fe9ccd60e97cd37a3c5b2aadf3ee71068f2d2f8
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2014-05-15 20:57:53 +0900

    SQL実行機能。

--- src/main/java/cherry/sqlapp/controller/secure/sqlexec/SqlExecController.java
+++ src/main/java/cherry/sqlapp/controller/secure/sqlexec/SqlExecController.java
@@ -25,6 +25,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -35,11 +37,38 @@ public interface SqlExecController {
 
 	public static final String URI_PATH = "/secure/sqlexec";
 
-	public static final String URI_PATH_REQ = "req";
+	public static final String URI_PATH_ID = "{id}";
+
+	public static final String URI_PATH_ID_UPDATE = "{id}/update";
+
+	public static final String URI_PATH_ID_UPDATE_S = "{id}/updateS";
+
+	public static final String URI_PATH_ID_UPDATE_A = "{id}/updateA";
+
+	public static final String URI_PATH_ID_UPDATE_C = "{id}/updateC";
+
+	public static final String URI_PATH_ID_EXEC = "{id}/exec";
+
+	public static final String PATH_VAR = "id";
 
 	public static final String PARAM_NO = "no";
 
 	public static final String PARAM_SZ = "sz";
+
+	@ModelAttribute("sqlExecSearchForm")
+	SqlExecSearchForm getSearcForm();
+
+	@ModelAttribute("sqlExecMetadataForm")
+	SqlExecMetadataForm getForm();
+
+	@ModelAttribute("sqlExecSelectForm")
+	SqlExecSelectForm getFormS();
+
+	@ModelAttribute("sqlExecAnyForm")
+	SqlExecAnyForm getFormA();
+
+	@ModelAttribute("sqlExecCsvForm")
+	SqlExecCsvForm getFormC();
 
 	@RequestMapping()
 	ModelAndView index(
@@ -47,6 +76,41 @@ public interface SqlExecController {
 			BindingResult binding,
 			@RequestParam(value = PARAM_NO, required = false, defaultValue = "0") int pageNo,
 			@RequestParam(value = PARAM_SZ, required = false, defaultValue = "0") int pageSz,
+			Authentication authentication, Locale locale,
+			SitePreference sitePreference, HttpServletRequest request);
+
+	@RequestMapping(URI_PATH_ID)
+	ModelAndView edit(@PathVariable(PATH_VAR) int id,
+			Authentication authentication, Locale locale,
+			SitePreference sitePreference, HttpServletRequest request);
+
+	@RequestMapping(URI_PATH_ID_UPDATE)
+	ModelAndView update(@PathVariable(PATH_VAR) int id,
+			@Validated SqlExecMetadataForm form, BindingResult binding,
+			Authentication authentication, Locale locale,
+			SitePreference sitePreference, HttpServletRequest request);
+
+	@RequestMapping(URI_PATH_ID_UPDATE_S)
+	ModelAndView updateS(@PathVariable(PATH_VAR) int id,
+			@Validated SqlExecSelectForm form, BindingResult binding,
+			Authentication authentication, Locale locale,
+			SitePreference sitePreference, HttpServletRequest request);
+
+	@RequestMapping(URI_PATH_ID_UPDATE_A)
+	ModelAndView updateA(@PathVariable(PATH_VAR) int id,
+			@Validated SqlExecAnyForm form, BindingResult binding,
+			Authentication authentication, Locale locale,
+			SitePreference sitePreference, HttpServletRequest request);
+
+	@RequestMapping(URI_PATH_ID_UPDATE_C)
+	ModelAndView updateC(@PathVariable(PATH_VAR) int id,
+			@Validated SqlExecCsvForm form, BindingResult binding,
+			Authentication authentication, Locale locale,
+			SitePreference sitePreference, HttpServletRequest request);
+
+	@RequestMapping(URI_PATH_ID_EXEC)
+	ModelAndView exec(@PathVariable(PATH_VAR) int id,
+			@Validated SqlExecCsvForm form, BindingResult binding,
 			Authentication authentication, Locale locale,
 			SitePreference sitePreference, HttpServletRequest request);
 
