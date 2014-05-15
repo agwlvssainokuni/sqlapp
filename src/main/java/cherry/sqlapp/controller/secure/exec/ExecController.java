b7d860c976ad587d3570598d399c6a21a789c1c1
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2014-05-15 22:43:33 +0900

    SQL実行機能。

--- src/main/java/cherry/sqlapp/controller/secure/exec/ExecController.java
+++ src/main/java/cherry/sqlapp/controller/secure/exec/ExecController.java
@@ -26,7 +26,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -35,40 +34,19 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping(ExecController.URI_PATH)
 public interface ExecController {
 
-	public static final String URI_PATH = "/secure/sqlexec";
+	public static final String URI_PATH = "/secure/exec";
 
-	public static final String URI_PATH_ID = "{id}";
-
-	public static final String URI_PATH_ID_UPDATE = "{id}/update";
-
-	public static final String URI_PATH_ID_UPDATE_S = "{id}/updateS";
-
-	public static final String URI_PATH_ID_UPDATE_A = "{id}/updateA";
-
-	public static final String URI_PATH_ID_UPDATE_C = "{id}/updateC";
-
-	public static final String URI_PATH_ID_EXEC = "{id}/exec";
-
-	public static final String PATH_VAR = "id";
+	public static final String URI_PATH_HISTORY = "history";
 
 	public static final String PARAM_NO = "no";
 
 	public static final String PARAM_SZ = "sz";
 
-	@ModelAttribute("sqlExecSearchForm")
-	ExecSearchForm getSearcForm();
+	@ModelAttribute("execSearchForm")
+	ExecSearchForm getSearchForm();
 
-	@ModelAttribute("sqlExecMetadataForm")
-	ExecMetadataForm getForm();
-
-	@ModelAttribute("sqlExecSelectForm")
-	ExecSelectForm getFormS();
-
-	@ModelAttribute("sqlExecAnyForm")
-	ExecAnyForm getFormA();
-
-	@ModelAttribute("sqlExecCsvForm")
-	ExecCsvForm getFormC();
+	@ModelAttribute("execHistoryForm")
+	ExecHistoryForm getHistoryForm();
 
 	@RequestMapping()
 	ModelAndView index(
@@ -79,38 +57,12 @@ public interface ExecController {
 			Authentication authentication, Locale locale,
 			SitePreference sitePreference, HttpServletRequest request);
 
-	@RequestMapping(URI_PATH_ID)
-	ModelAndView edit(@PathVariable(PATH_VAR) int id,
-			Authentication authentication, Locale locale,
-			SitePreference sitePreference, HttpServletRequest request);
-
-	@RequestMapping(URI_PATH_ID_UPDATE)
-	ModelAndView update(@PathVariable(PATH_VAR) int id,
-			@Validated ExecMetadataForm form, BindingResult binding,
-			Authentication authentication, Locale locale,
-			SitePreference sitePreference, HttpServletRequest request);
-
-	@RequestMapping(URI_PATH_ID_UPDATE_S)
-	ModelAndView updateS(@PathVariable(PATH_VAR) int id,
-			@Validated ExecSelectForm form, BindingResult binding,
-			Authentication authentication, Locale locale,
-			SitePreference sitePreference, HttpServletRequest request);
-
-	@RequestMapping(URI_PATH_ID_UPDATE_A)
-	ModelAndView updateA(@PathVariable(PATH_VAR) int id,
-			@Validated ExecAnyForm form, BindingResult binding,
-			Authentication authentication, Locale locale,
-			SitePreference sitePreference, HttpServletRequest request);
-
-	@RequestMapping(URI_PATH_ID_UPDATE_C)
-	ModelAndView updateC(@PathVariable(PATH_VAR) int id,
-			@Validated ExecCsvForm form, BindingResult binding,
-			Authentication authentication, Locale locale,
-			SitePreference sitePreference, HttpServletRequest request);
-
-	@RequestMapping(URI_PATH_ID_EXEC)
-	ModelAndView exec(@PathVariable(PATH_VAR) int id,
-			@Validated ExecCsvForm form, BindingResult binding,
+	@RequestMapping(URI_PATH_HISTORY)
+	ModelAndView history(
+			@Validated ExecHistoryForm form,
+			BindingResult binding,
+			@RequestParam(value = PARAM_NO, required = false, defaultValue = "0") int pageNo,
+			@RequestParam(value = PARAM_SZ, required = false, defaultValue = "0") int pageSz,
 			Authentication authentication, Locale locale,
 			SitePreference sitePreference, HttpServletRequest request);
 
