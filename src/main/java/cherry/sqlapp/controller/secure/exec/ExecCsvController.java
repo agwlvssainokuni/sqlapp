a5dbb8aadaf280a1be2ff130fbfe3a09229cfdc1
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2014-06-07 16:04:06 +0900

    SQL実行機能。

--- src/main/java/cherry/sqlapp/controller/secure/exec/ExecCsvController.java
+++ src/main/java/cherry/sqlapp/controller/secure/exec/ExecCsvController.java
@@ -22,7 +22,6 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.mobile.device.site.SitePreference;
 import org.springframework.security.core.Authentication;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -31,7 +30,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
 @RequestMapping(ExecCsvController.URI_PATH)
 public interface ExecCsvController {
 
