1e8115fb42b2fe3d617250a5724a01a6f979be24
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2014-06-19 00:15:49 +0900

    SQL実行機能 * URIパス変更

--- src/main/resources/db/migration/V00000000_0__SetupSchema.sql
+++ src/main/resources/db/migration/V00000000_0__SetupSchema.sql
@@ -1,22 +1,22 @@
 -- Project Name : SqlApp
--- Date/Time    : 2014/06/17 6:39:12
+-- Date/Time    : 2014/06/19 0:14:42
 -- Author       : agwlvssainokuni
 -- RDBMS Type   : IBM DB2
 -- Application  : A5:SQL Mk-2
 
--- SQL文・CSV取込み
-CREATE TABLE sql_csv( 
+-- SQLツール・CSV取込み
+CREATE TABLE sqltool_load( 
 	id INTEGER NOT NULL auto_increment, 
 	database_name VARCHAR (50) DEFAULT 'default' NOT NULL, 
 	query VARCHAR (5000) NOT NULL, 
 	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, 
 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, 
 	deleted_flg INTEGER DEFAULT 0 NOT NULL, 
-	CONSTRAINT sql_csv_pkc PRIMARY KEY (id)
+	CONSTRAINT sqltool_load_pkc PRIMARY KEY (id)
 ); 
 
--- SQL文・一般クエリ
-CREATE TABLE sql_any( 
+-- SQLツール・ステートメント指定
+CREATE TABLE sqltool_statement( 
 	id INTEGER NOT NULL auto_increment, 
 	database_name VARCHAR (50) DEFAULT 'default' NOT NULL, 
 	query VARCHAR (5000) NOT NULL, 
@@ -24,11 +24,11 @@ CREATE TABLE sql_any(
 	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, 
 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, 
 	deleted_flg INTEGER DEFAULT 0 NOT NULL, 
-	CONSTRAINT sql_any_pkc PRIMARY KEY (id)
+	CONSTRAINT sqltool_statement_pkc PRIMARY KEY (id)
 ); 
 
--- SQL文・SELECT
-CREATE TABLE sql_select( 
+-- SQLツール・句指定
+CREATE TABLE sqltool_clause( 
 	id INTEGER NOT NULL auto_increment, 
 	database_name VARCHAR (50) DEFAULT 'default' NOT NULL, 
 	select_clause VARCHAR (500) NOT NULL, 
@@ -41,11 +41,11 @@ CREATE TABLE sql_select(
 	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, 
 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, 
 	deleted_flg INTEGER DEFAULT 0 NOT NULL, 
-	CONSTRAINT sql_select_pkc PRIMARY KEY (id)
+	CONSTRAINT sqltool_clause_pkc PRIMARY KEY (id)
 ); 
 
--- SQLメタデータ
-CREATE TABLE sql_metadata( 
+-- SQLツール・メタデータ
+CREATE TABLE sqltool_metadata( 
 	id INTEGER NOT NULL auto_increment, 
 	sql_type VARCHAR (32) NOT NULL, 
 	name VARCHAR (50) DEFAULT CURRENT_TIMESTAMP NOT NULL, 
@@ -56,124 +56,124 @@ CREATE TABLE sql_metadata(
 	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, 
 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, 
 	deleted_flg INTEGER DEFAULT 0 NOT NULL, 
-	CONSTRAINT sql_metadata_pkc PRIMARY KEY (id)
+	CONSTRAINT sqltool_metadata_pkc PRIMARY KEY (id)
 ); 
 
 COMMENT 
-	ON TABLE sql_csv IS 'SQL文・CSV取込み'; 
+	ON TABLE sqltool_load IS 'SQLツール・CSV取込み'; 
 
 COMMENT 
-	ON COLUMN sql_csv.id IS 'ID'; 
+	ON COLUMN sqltool_load.id IS 'ID'; 
 
 COMMENT 
-	ON COLUMN sql_csv.database_name IS 'DB名'; 
+	ON COLUMN sqltool_load.database_name IS 'DB名'; 
 
 COMMENT 
-	ON COLUMN sql_csv.query IS 'クエリ'; 
+	ON COLUMN sqltool_load.query IS 'クエリ'; 
 
 COMMENT 
-	ON COLUMN sql_csv.updated_at IS '更新日時'; 
+	ON COLUMN sqltool_load.updated_at IS '更新日時'; 
 
 COMMENT 
-	ON COLUMN sql_csv.created_at IS '作成日時'; 
+	ON COLUMN sqltool_load.created_at IS '作成日時'; 
 
 COMMENT 
-	ON COLUMN sql_csv.deleted_flg IS '削除フラグ'; 
+	ON COLUMN sqltool_load.deleted_flg IS '削除フラグ'; 
 
 COMMENT 
-	ON TABLE sql_any IS 'SQL文・一般クエリ'; 
+	ON TABLE sqltool_statement IS 'SQLツール・ステートメント指定'; 
 
 COMMENT 
-	ON COLUMN sql_any.id IS 'ID'; 
+	ON COLUMN sqltool_statement.id IS 'ID'; 
 
 COMMENT 
-	ON COLUMN sql_any.database_name IS 'DB名'; 
+	ON COLUMN sqltool_statement.database_name IS 'DB名'; 
 
 COMMENT 
-	ON COLUMN sql_any.query IS 'クエリ'; 
+	ON COLUMN sqltool_statement.query IS 'クエリ'; 
 
 COMMENT 
-	ON COLUMN sql_any.param_map IS 'パラメタMAP(JSON)'; 
+	ON COLUMN sqltool_statement.param_map IS 'パラメタMAP(JSON)'; 
 
 COMMENT 
-	ON COLUMN sql_any.updated_at IS '更新日時'; 
+	ON COLUMN sqltool_statement.updated_at IS '更新日時'; 
 
 COMMENT 
-	ON COLUMN sql_any.created_at IS '作成日時'; 
+	ON COLUMN sqltool_statement.created_at IS '作成日時'; 
 
 COMMENT 
-	ON COLUMN sql_any.deleted_flg IS '削除フラグ'; 
+	ON COLUMN sqltool_statement.deleted_flg IS '削除フラグ'; 
 
 COMMENT 
-	ON TABLE sql_select IS 'SQL文・SELECT'; 
+	ON TABLE sqltool_clause IS 'SQLツール・句指定'; 
 
 COMMENT 
-	ON COLUMN sql_select.id IS 'ID'; 
+	ON COLUMN sqltool_clause.id IS 'ID'; 
 
 COMMENT 
-	ON COLUMN sql_select.database_name IS 'DB名'; 
+	ON COLUMN sqltool_clause.database_name IS 'DB名'; 
 
 COMMENT 
-	ON COLUMN sql_select.select_clause IS 'SELECT句'; 
+	ON COLUMN sqltool_clause.select_clause IS 'SELECT句'; 
 
 COMMENT 
-	ON COLUMN sql_select.from_clause IS 'FROM句'; 
+	ON COLUMN sqltool_clause.from_clause IS 'FROM句'; 
 
 COMMENT 
-	ON COLUMN sql_select.where_clause IS 'WHERE句'; 
+	ON COLUMN sqltool_clause.where_clause IS 'WHERE句'; 
 
 COMMENT 
-	ON COLUMN sql_select.group_by_clause IS 'GROUP BY句'; 
+	ON COLUMN sqltool_clause.group_by_clause IS 'GROUP BY句'; 
 
 COMMENT 
-	ON COLUMN sql_select.having_clause IS 'HAVING句'; 
+	ON COLUMN sqltool_clause.having_clause IS 'HAVING句'; 
 
 COMMENT 
-	ON COLUMN sql_select.order_by_clause IS 'ORDER BY句'; 
+	ON COLUMN sqltool_clause.order_by_clause IS 'ORDER BY句'; 
 
 COMMENT 
-	ON COLUMN sql_select.param_map IS 'パラメタMAP(JSON)'; 
+	ON COLUMN sqltool_clause.param_map IS 'パラメタMAP(JSON)'; 
 
 COMMENT 
-	ON COLUMN sql_select.updated_at IS '更新日時'; 
+	ON COLUMN sqltool_clause.updated_at IS '更新日時'; 
 
 COMMENT 
-	ON COLUMN sql_select.created_at IS '作成日時'; 
+	ON COLUMN sqltool_clause.created_at IS '作成日時'; 
 
 COMMENT 
-	ON COLUMN sql_select.deleted_flg IS '削除フラグ'; 
+	ON COLUMN sqltool_clause.deleted_flg IS '削除フラグ'; 
 
 COMMENT 
-	ON TABLE sql_metadata IS 'SQLメタデータ'; 
+	ON TABLE sqltool_metadata IS 'SQLツール・メタデータ'; 
 
 COMMENT 
-	ON COLUMN sql_metadata.id IS 'ID'; 
+	ON COLUMN sqltool_metadata.id IS 'ID'; 
 
 COMMENT 
-	ON COLUMN sql_metadata.sql_type IS 'SQL種別'; 
+	ON COLUMN sqltool_metadata.sql_type IS 'SQL種別'; 
 
 COMMENT 
-	ON COLUMN sql_metadata.name IS '表示名'; 
+	ON COLUMN sqltool_metadata.name IS '表示名'; 
 
 COMMENT 
-	ON COLUMN sql_metadata.description IS '説明文'; 
+	ON COLUMN sqltool_metadata.description IS '説明文'; 
 
 COMMENT 
-	ON COLUMN sql_metadata.owned_by IS '所有者'; 
+	ON COLUMN sqltool_metadata.owned_by IS '所有者'; 
 
 COMMENT 
-	ON COLUMN sql_metadata.published_flg IS '公開フラグ'; 
+	ON COLUMN sqltool_metadata.published_flg IS '公開フラグ'; 
 
 COMMENT 
-	ON COLUMN sql_metadata.registered_at IS '登録日時'; 
+	ON COLUMN sqltool_metadata.registered_at IS '登録日時'; 
 
 COMMENT 
-	ON COLUMN sql_metadata.updated_at IS '更新日時'; 
+	ON COLUMN sqltool_metadata.updated_at IS '更新日時'; 
 
 COMMENT 
-	ON COLUMN sql_metadata.created_at IS '作成日時'; 
+	ON COLUMN sqltool_metadata.created_at IS '作成日時'; 
 
 COMMENT 
-	ON COLUMN sql_metadata.deleted_flg IS '削除フラグ'; 
+	ON COLUMN sqltool_metadata.deleted_flg IS '削除フラグ'; 
 
 
