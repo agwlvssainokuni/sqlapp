17b6ea3de249c434696405714f8d96c3c4fd41d6
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2014-06-20 07:09:24 +0900

    SQL実行機能 * ダウンロード機能。

--- src/main/java/cherry/sqlapp/service/sqltool/exec/ExecQueryService.java
+++ src/main/java/cherry/sqlapp/service/sqltool/exec/ExecQueryService.java
@@ -18,7 +18,16 @@ package cherry.sqlapp.service.sqltool.exec;
 
 import java.util.Map;
 
+import cherry.spring.common.lib.etl.Consumer;
+import cherry.spring.common.lib.paginate.PageSet;
+
 public interface ExecQueryService {
+
+	PageSet query(String databaseName, String sql, Map<String, ?> paramMap,
+			Consumer consumer);
+
+	PageSet query(String databaseName, QueryBuilder queryBuilder,
+			Map<String, ?> paramMap, int pageNo, int pageSz, Consumer consumer);
 
 	Result query(String databaseName, String sql, Map<String, ?> paramMap);
 
