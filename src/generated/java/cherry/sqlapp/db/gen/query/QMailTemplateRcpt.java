64297338ae1c4623bd2f698e8c000878ea3bae8b
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2015-01-10 09:19:59 +0900

    SQL実行機能 * スキーマ最新化。

--- src/generated/java/cherry/sqlapp/db/gen/query/QMailTemplateRcpt.java
+++ src/generated/java/cherry/sqlapp/db/gen/query/QMailTemplateRcpt.java
@@ -15,14 +15,14 @@ import java.sql.Types;
 
 
 /**
- * QMailTemplateAddress is a Querydsl query type for BMailTemplateAddress
+ * QMailTemplateRcpt is a Querydsl query type for BMailTemplateRcpt
  */
 @Generated("com.mysema.query.sql.codegen.MetaDataSerializer")
-public class QMailTemplateAddress extends com.mysema.query.sql.RelationalPathBase<BMailTemplateAddress> {
+public class QMailTemplateRcpt extends com.mysema.query.sql.RelationalPathBase<BMailTemplateRcpt> {
 
-    private static final long serialVersionUID = 260936975;
+    private static final long serialVersionUID = 1271949498;
 
-    public static final QMailTemplateAddress mailTemplateAddress = new QMailTemplateAddress("MAIL_TEMPLATE_ADDRESS");
+    public static final QMailTemplateRcpt mailTemplateRcpt = new QMailTemplateRcpt("MAIL_TEMPLATE_RCPT");
 
     public final DateTimePath<org.joda.time.LocalDateTime> createdAt = createDateTime("createdAt", org.joda.time.LocalDateTime.class);
 
@@ -40,25 +40,25 @@ public class QMailTemplateAddress extends com.mysema.query.sql.RelationalPathBas
 
     public final DateTimePath<org.joda.time.LocalDateTime> updatedAt = createDateTime("updatedAt", org.joda.time.LocalDateTime.class);
 
-    public final com.mysema.query.sql.PrimaryKey<BMailTemplateAddress> mailTemplateAddressPkc = createPrimaryKey(id);
+    public final com.mysema.query.sql.PrimaryKey<BMailTemplateRcpt> mailTemplateRcptPkc = createPrimaryKey(id);
 
-    public QMailTemplateAddress(String variable) {
-        super(BMailTemplateAddress.class, forVariable(variable), "PUBLIC", "MAIL_TEMPLATE_ADDRESS");
+    public QMailTemplateRcpt(String variable) {
+        super(BMailTemplateRcpt.class, forVariable(variable), "PUBLIC", "MAIL_TEMPLATE_RCPT");
         addMetadata();
     }
 
-    public QMailTemplateAddress(String variable, String schema, String table) {
-        super(BMailTemplateAddress.class, forVariable(variable), schema, table);
+    public QMailTemplateRcpt(String variable, String schema, String table) {
+        super(BMailTemplateRcpt.class, forVariable(variable), schema, table);
         addMetadata();
     }
 
-    public QMailTemplateAddress(Path<? extends BMailTemplateAddress> path) {
-        super(path.getType(), path.getMetadata(), "PUBLIC", "MAIL_TEMPLATE_ADDRESS");
+    public QMailTemplateRcpt(Path<? extends BMailTemplateRcpt> path) {
+        super(path.getType(), path.getMetadata(), "PUBLIC", "MAIL_TEMPLATE_RCPT");
         addMetadata();
     }
 
-    public QMailTemplateAddress(PathMetadata<?> metadata) {
-        super(BMailTemplateAddress.class, metadata, "PUBLIC", "MAIL_TEMPLATE_ADDRESS");
+    public QMailTemplateRcpt(PathMetadata<?> metadata) {
+        super(BMailTemplateRcpt.class, metadata, "PUBLIC", "MAIL_TEMPLATE_RCPT");
         addMetadata();
     }
 
@@ -67,7 +67,7 @@ public class QMailTemplateAddress extends com.mysema.query.sql.RelationalPathBas
         addMetadata(deletedFlg, ColumnMetadata.named("DELETED_FLG").withIndex(8).ofType(Types.INTEGER).withSize(10).notNull());
         addMetadata(id, ColumnMetadata.named("ID").withIndex(1).ofType(Types.BIGINT).withSize(19).notNull());
         addMetadata(lockVersion, ColumnMetadata.named("LOCK_VERSION").withIndex(7).ofType(Types.INTEGER).withSize(10).notNull());
-        addMetadata(rcptAddr, ColumnMetadata.named("RCPT_ADDR").withIndex(4).ofType(Types.VARCHAR).withSize(512).notNull());
+        addMetadata(rcptAddr, ColumnMetadata.named("RCPT_ADDR").withIndex(4).ofType(Types.VARCHAR).withSize(300).notNull());
         addMetadata(rcptType, ColumnMetadata.named("RCPT_TYPE").withIndex(3).ofType(Types.VARCHAR).withSize(3).notNull());
         addMetadata(templateId, ColumnMetadata.named("TEMPLATE_ID").withIndex(2).ofType(Types.BIGINT).withSize(19).notNull());
         addMetadata(updatedAt, ColumnMetadata.named("UPDATED_AT").withIndex(5).ofType(Types.TIMESTAMP).withSize(23).withDigits(10).notNull());
