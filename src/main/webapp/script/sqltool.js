a197dcb4169b7b8e9ec88329544569c1550e0d0b
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2015-01-24 06:52:23 +0900

    SQL実行機能 * ページネーションリンク。

--- src/main/webapp/script/sqltool.js
+++ src/main/webapp/script/sqltool.js
@@ -23,4 +23,10 @@ $(function() {
 		})
 	});
 
+	var token = $("meta[name='_csrf']").attr("content");
+	var header = $("meta[name='_csrf_header']").attr("content");
+	$(document).ajaxSend(function(event, jqxhr, settings) {
+		jqxhr.setRequestHeader(header, token);
+	});
+
 });
